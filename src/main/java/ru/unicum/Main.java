14dda378470e31806fcb2db8efe9296f85d8ef76
Egor Tellin <egor21chuv@yandex.ru>
2023-09-26 08:30:57 +0300

    hello message was rechanged2

--- src/main/java/ru/unicum/Main.java
+++ src/main/java/ru/unicum/Main.java
@@ -2,6 +2,6 @@ package ru.unicum;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello EgrTell");
+        System.out.println("Hello EgrTell123");
     }
 }
